cc9a8603038e5e3ed6c7bbae5bbb1dbfc2e64cfe
Maxi Rubio <maxirubio92@gmail.com>
2022-10-25 16:23:14 -0300

    NavBar

---
+++ src/components/NavBar.js
@@ -0,0 +1,23 @@
+const NavBar = () => {
+    return (
+    <header>    
+        <nav>
+            <h1>NEW MAX</h1>
+            <div>
+              <ul>
+                <li>
+                  <a href="index.html">INICIO</a>
+                </li>
+                <li>
+                  <a href="pages/productos.html">PRODUCTOS</a>
+                </li>
+                <li>
+                  <a href="pages/info.html">¿QUIÉNES SOMOS?</a>
+                </li>
+              </ul>
+            </div>
+          </nav>
+    </header>
+)}
+
+export default NavBar
